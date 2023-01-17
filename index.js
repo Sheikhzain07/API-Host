521218429ea4772dcd67df6f5c2a53ab4045b615
Sheikhzain07 <sheikhzain459@gmail.com>
2023-01-18 03:09:28 +0530

    api host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDb = require("./connect/db"); //it returns promise
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const routers = require("./routes/products.routes");
 app.get("/", (req, res) => {
   res.send("Server is live");
