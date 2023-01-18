02aa7d13f0be4e9670d031fd70f5be3fff547d07
Sheikhzain07 <sheikhzain459@gmail.com>
2023-01-18 13:14:35 +0530

    api host changes

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { getAllProducts } = require("../controllers/products.controllers");
 
-router.route("/").get(getAllProducts);
+router.route("/").get(getAllProducts); //at this router we call that function
 module.exports = router;
